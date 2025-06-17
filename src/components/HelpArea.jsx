5e4f6c1ffca3564d049ae117beaa23b6761fdc21
Nishu <nishanthnayaka007@gmail.com>
2025-06-17 17:28:23 +0530

    adding comments [skip ci]

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -3,7 +3,6 @@ import './HelpArea.css';
 
 const HELP_ITEMS = [
   {
-    // 
     id: 'h1',
     title: 'What is Git?',
     text: 'Git is a version control system, helping you to manage your code and create code snapshots.',
